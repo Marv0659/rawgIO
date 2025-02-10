27e5a2c9f3472a8350e0ec8cb089b01df36cfcb3
Marvin Audisho <maaud0659@stud.kea.dk>
2025-02-10 11:18:47 +0100

    add ColorModeSwitch component and integrate it into NavBar; configure Chakra UI theme for dark mode

--- rawg-client/src/components/NavBar.tsx
+++ rawg-client/src/components/NavBar.tsx
@@ -1,12 +1,14 @@
-import { HStack,Text, Image } from '@chakra-ui/react'
+import { HStack,Text, Image, Switch } from '@chakra-ui/react'
 import React from 'react'
 import logo from '../assets/logo.png'
+import ColorModeSwitch from './ColorModeSwitch'
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
       <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   )
 }
